d2cb21a7184fb401502a631984babfa5a9ed800e
aminelebrini <aminelebrini4@gmail.com>
2026-02-18 14:14:04 +0000

    la creation des tableaux de migration

---
+++ backend/database/migrations/2026_02_18_111219_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->fullname();
+            $table->username();
+            $table->link_profile();
+            $table->email();
+            $table->password();
+            $table->role();
+            $table->ville();
+            $table->status();
+            $table->etat();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
