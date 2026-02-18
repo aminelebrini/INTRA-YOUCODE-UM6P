f66f6a72645cf5fb879abc97bf7bbdf0d83267f9
aminelebrini <aminelebrini4@gmail.com>
2026-02-18 14:14:04 +0000

    la creation des tableaux de migration

---
+++ backend/database/migrations/2026_02_18_141058_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->integer('points');
+            $table->string('promotion');
+            $table->string('campus');
+            $table->string('annee');
+            $table->foreignId('classe_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
