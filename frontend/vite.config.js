0b048734cfbd648f44b3951dfd519083007b2efd
AMINE LEBRINI <aminelebrini4@gmail.com>
2026-03-27 20:00:54 +0000

    fixation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -15,4 +14,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+  }
 })
