e5a4bbd30f6001cff615ae73c4a34455206257c9
AMINE LEBRINI <aminelebrini4@gmail.com>
2026-04-26 06:48:55 +0100

    fixing big error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiBaseURL = import.meta.env.VITE_API_URL ?? 'http://localhost/api';
+
 const api = axios.create({
-    baseURL: 'http://intra-youcode.me/api',
+    baseURL: apiBaseURL,
     withCredentials: false,
     withXSRFToken: false,
 });
